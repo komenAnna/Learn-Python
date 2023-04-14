7bdcb119d1113196f075e01540b4061b2ab6dcf0
komenAnna <komenriana@gmail.com>
2023-04-14 22:58:45 +0300

    Added a new file that will host the challenges

---
+++ basics.py
@@ -0,0 +1,7 @@
+#Challenge 1: Print "Hello, World!" to the console
+print("Hello World. I'm listening to 'Ghost' by Justin Beiber!")
+
+#Challenge 2: Print your name to the console
+print("My name is Marianna Komen and i aspire to be a good programmer/data scientist")
+
+#Create a variable and assign it a value
