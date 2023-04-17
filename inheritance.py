62fe71356f4013fdd1341ca6a855c81de7048853
komenAnna <komenriana@gmail.com>
2023-04-17 22:23:31 +0300

    tried to understand the code --- need more learning on inheritance

--- inheritance.py
+++ inheritance.py
@@ -3,6 +3,7 @@ class Polygon:
     def __init__(self, no_of_sides):
         self.n = no_of_sides
         self.sides = [0 for i in range(no_of_sides)]
+        
 
     def inputSides(self):
         self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
@@ -39,3 +40,4 @@ t.dispSides()
 
 # Calculating and printing the area of the triangle
 t.findArea()
+
